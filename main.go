5c35e25b3ada03f28bc794c951a5d2d6ad127cfc
drighetto <drighetto@excellium-services.com>
2023-04-18 16:19:56 +0200

    Add info until now

--- main.go
+++ main.go
@@ -31,13 +31,18 @@ func main() {
 	//See https://stackoverflow.com/a/73880886 for the detail of the trick
 	var daysInMonth = 32 - (time.Date(currentTime.Year(), currentTime.Month(), 32, 0, 0, 0, 0, loc)).Day()
 	var workingDaysCount = 0
+	var workingDaysCountUntilNow = 0
 	for d := 1; d <= daysInMonth; d++ {
 		var currentDay = time.Date(currentTime.Year(), currentTime.Month(), d, 0, 0, 0, 0, loc)
 		var currentDayStr = currentDay.Format("20060102")
 		if currentDay.Weekday() != time.Saturday && currentDay.Weekday() != time.Sunday && !slices.Contains(publicHolidays, currentDayStr) {
 			workingDaysCount++
+			if currentTime.Day() >= d {
+				workingDaysCountUntilNow++
+			}
 		}
 	}
 	color.Cyan("[+] %s information:\n", currentTime.Format("January"))
-	fmt.Printf("%d working days so %d working hours in total.\n", workingDaysCount, workingDaysCount*8)
+	fmt.Printf("%-02d working days so %-03d working hours until now.\n", workingDaysCountUntilNow, workingDaysCountUntilNow*8)
+	fmt.Printf("%-02d working days so %-03d working hours in total.\n", workingDaysCount, workingDaysCount*8)
 }
